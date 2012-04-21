64cb57be19e5952ac036831f2494b07f199cefbb
icd3 <git@iandimayuga.com>
2012-04-21 21:36:27 +0000

    Last commit

--- src/search.py
+++ src/search.py
@@ -3,17 +3,17 @@ import json
 import re
 r={}
 t=sys.stdout
+def w(p):t.write(p);t.flush()
 def x(f):
  for l in f:
   l=l.strip().partition(':')
   if l[2]:r[c(l[2])]=l[0].strip()
 def c(t):return re.sub(r'([\?!-])',' \1',re.sub(r'([/\(\):\.,;])','',t)).lower()
 x(open(sys.argv[1],'r'))
-t.flush()
 while 1:
- t.write('> '); t.flush()
+ w('> ')
  try:i=sys.stdin.readline()
- except:break;
+ except:break
  if i:
   i=c(i).split()
   s=[]
@@ -21,5 +21,4 @@ while 1:
    for u in i:
     if not u in k:break
    else:s.append(r[k])
-  t.write(json.dumps(s))
-  t.flush()
+  w(json.dumps(s))
