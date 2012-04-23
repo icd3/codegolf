eb9d1b0a8dfce8e3f5397029b47ec6bdf8a2b929
icd3 <git@iandimayuga.com>
2012-04-23 23:49:47 +0000

    Condense regex

--- src/search.py
+++ src/search.py
@@ -7,7 +7,7 @@ def x(f):
   l=l.strip().partition(' : ');s=l[0];t=c(l[2])
   if t in r:r[t].append(s)
   else:r[t]=[s]
-def c(t):return re.sub(r'([-])',' \1 ',re.sub(r'([?!])',' \1',re.sub(r'([/():.,;])','',t.lower())))
+def c(t):return re.sub(r'([-?!])',' \1',re.sub(r'([/():.,;])','',t.lower()))
 x(open(sys.argv[1],'r'))
 while 1:
  w('> ')
