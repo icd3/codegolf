c9f0e407b2295c3bd5de858f75ef0ddbda0cd79d
icd3 <git@iandimayuga.com>
2012-04-23 23:41:07 +0000

    Fix duplicate review bug

--- src/search.py
+++ src/search.py
@@ -1,24 +1,22 @@
-import sys
-import json
-import re
+import sys,json,re
 r={}
 t=sys.stdout
 def w(p):t.write(p);t.flush()
 def x(f):
  for l in f:
-  l=l.strip().partition(':')
-  if l[2]:r[c(l[2])]=l[0].strip()
-def c(t):return re.sub(r'([\?!-])',' \1',re.sub(r'([/\(\):\.,;])','',t)).lower()
+  l=l.strip().partition(' : ');s=l[0];t=c(l[2])
+  if t in r:r[t].append(s)
+  else:r[t]=[s]
+def c(t):return re.sub(r'([-])',' \1 ',re.sub(r'([?!])',' \1',re.sub(r'([/():.,;])','',t.lower())))
 x(open(sys.argv[1],'r'))
 while 1:
  w('> ')
- try:i=sys.stdin.readline()
- except:break
+ i=sys.stdin.readline()
  if i:
   i=c(i).split()
   s=[]
   for k in r.keys():
    for u in i:
     if not u in k:break
-   else:s.append(r[k])
+   else:s.extend(r[k])
   w(json.dumps(s))
