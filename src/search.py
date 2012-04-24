b833594ff1f34a71766453951721c066bffaa557
icd3 <git@iandimayuga.com>
2012-04-24 00:01:34 +0000

    Remove unnecessary strip()

--- src/search.py
+++ src/search.py
@@ -4,17 +4,15 @@ t=sys.stdout
 def w(p):t.write(p);t.flush()
 def x(f):
  for l in f:
-  l=l.strip().partition(' : ');s=l[0];t=c(l[2])
+  l=l.partition(' : ');s=l[0];t=c(l[2])
   if t in r:r[t].append(s)
   else:r[t]=[s]
 def c(t):return re.sub(r'([-?!])',' \1',re.sub(r'([/():.,;])','',t.lower()))
 x(open(sys.argv[1],'r'))
 while 1:
- w('> ')
- i=sys.stdin.readline()
+ w('> ');i=sys.stdin.readline()
  if i:
-  i=c(i).split()
-  s=[]
+  i=c(i).split();s=[]
   for k in r.keys():
    for u in i:
     if not u in k:break
